e963cbb18995dc01ce9749d002c61fb13e79d8fc
Leandro <leandro.gsilva@fde.sp.gov.br>
2020-05-05 11:30:26 -0300

    Conclusão da listagem de repositórios

--- script/install.sh
+++ script/install.sh
@@ -5,5 +5,5 @@ yarn add eslint -D
 yarn eslint --init
 yarn
 yarn add eslint-config-prettier eslint-plugin-prettier babel-eslint -D
-yarn add react-router-dom styled-components react-icons axios
+yarn add react-router-dom styled-components react-icons axios prop-types
 
--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
 
-// import { Container } from './styles';
+import { Loading, Ouwner, IssueList } from './styles';
+import { Container } from '../../components/Container';
 
 export default class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string,
+      }),
+    }).isRequired,
+  };
+
   constructor(props) {
     super(props);
     this.state = {
@@ -38,7 +49,37 @@ export default class Repository extends Component {
   render() {
     const { repository, issues, loading } = this.state;
 
-    return <h1>Repo</h1>;
+    if (loading) {
+      return <Loading>Carregando</Loading>;
+    }
+
+    return (
+      <Container>
+        <Ouwner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Ouwner>
+
+        <IssueList>
+          {issues.map((issue) => (
+            <li kye={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map((label) => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          ))}
+        </IssueList>
+      </Container>
+    );
   }
 }
 
